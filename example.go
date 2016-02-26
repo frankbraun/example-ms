30fb1c622fe4625a2414b995523b88a4cf99377d
Frank Braun <frank@cryptogroup.net>
2016-02-26 14:47:16 +0000

    Add example microservice

---
+++ example.go
@@ -0,0 +1,47 @@
+package example
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+const (
+	MicroServiceName = "EXAMPLE"
+)
+
+func generateJsonResponse(w http.ResponseWriter, payload []byte, status int) {
+
+	// write the Header Content-Type to json
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	// add the status Header
+	w.WriteHeader(status)
+
+	// write the marshalled payload
+	w.Write(payload)
+
+}
+
+// API GET "/service/status"
+// Expect a 200 StatusOK HTTP Response
+func serviceStatus(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+
+	type serviceStatus struct {
+		Message string `json:"message"`
+		Uptime  string `json:"uptime"`
+	}
+
+	// find duration for the upTime timestamp
+	the_duration := time.Since(time.Unix(upTime, 0))
+
+	// response
+	st := &serviceStatus{
+		Message: "Micro Service " + MicroServiceName + " Status is OK",
+		Uptime:  the_duration.String(),
+	}
+	payload, _ := json.Marshal(st)
+	generateJsonResponse(w, payload, http.StatusOK)
+}
---
+++ http.go
@@ -0,0 +1,19 @@
+package example
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var upTime = time.Now().UTC().Unix()
+
+func init() {
+	r := httprouter.New()
+
+	// routes
+	r.GET("/service/status", serviceStatus)
+
+	http.Handle("/", r)
+}
